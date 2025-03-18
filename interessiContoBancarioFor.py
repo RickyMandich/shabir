f94c1821ab30e784844d4e14a03d47135d5a8596
RickyMandich <ricky.mandich@gmail.com>
2025-03-18 16:45:57 +0100

    aggiornamento 2025 03 18 16:45

--- interessiContoBancarioFor.py
+++
@@ -1,5 +0,0 @@
-# Esercizio
-# Si depositano 10 000€ in un conto bancario, remunerato con un interesse annuo del 5%. Quanti anni occorrono perchè il saldo del conto diventi il doppio dell'investimento iniziale?
-#
-# Scrivi un programma che legge da tastiera l'investimento iniziale e stampi il numero di anni necessari
-
